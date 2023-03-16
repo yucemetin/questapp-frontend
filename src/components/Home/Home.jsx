8ed9b80aff00b5f6615637cb5cbcd86cb030dea2
yucemetin <red.banana988@gmail.com>
2023-03-17 00:59:00 +0300

    material ui components added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 import Post from '../Post/Post'
 
 export default function Home() {
+    const [posts, setPosts] = useState([])
+    const [isLoaded, setIsLoaded] = useState()
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/v1/posts")
+            .then(response => {
+                setPosts(response)
+                setIsLoaded(true)
+            })
+            .catch(err => {
+                setIsLoaded(true)
+                setError(err)
+            })
+    }, [])
     return (
-        <div>
-            <Post />
+        <div className='px-40 py-10'>
+            {error && (
+                <div> {error}</div>
+            )}
+            {!isLoaded && (
+                <div> Loading...</div>
+            )}
+            {!error && isLoaded && posts.data.map(post => (
+                <Post key={post.id} title={post.title} text={post.text} />
+            ))}
         </div>
+
     )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,45 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import { useState } from "react";
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export default function Post() {
+export default function Post(props) {
 
-    const [posts, setPosts] = useState([])
-    const [isLoaded, setIsLoaded] = useState()
-    const [error, setError] = useState()
+    const { title, text } = props
+    const [expanded, setExpanded] = useState(false);
 
-    useEffect(() => {
-        axios.get("http://localhost:8080/api/v1/posts")
-            .then(response => {
-                setPosts(response)
-                setIsLoaded(true)
-            })
-            .catch(err => {
-                setIsLoaded(true)
-                setError(err)
-            })
-    }, [])
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
 
     return (
-        <div className='px-40 py-10'>
-            {error && (
-                <div> {error}</div>
-            )}
-            {!isLoaded && (
-                <div> Loading...</div>
-            )}
-            {!error && isLoaded && (
-                <ul className='flex flex-col gap-4'>
-                    {posts.data.map(post => (
-                        <li key={post.id} className="flex flex-col p-6 bg-red-200 rounded-3xl gap-y-4">
-                            <div className='text-red-400 font-extrabold text-2xl px-2'>{post.title}</div>
-                            <div className='rounded-2xl bg-red-300 p-4 flex justify-between text-red-600'>
-                                <div className='text-md font-bold text-white'>{post.text}</div>
-                                <div>1</div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={title}
+                subheader="September 14, 2016"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                   {text}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <ExpandMoreIcon onClick={handleExpandClick} />
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Method:</Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
     )
 }
